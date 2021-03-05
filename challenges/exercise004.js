44d9f4fe723fd0eb031acb4fc1ccf0db702ff1e1
Rana Alqrenawi <rana@gazaskygeeks.com>
2021-03-05 18:47:01 +0000

    exe4 pass through

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,43 +1,52 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  return nums.filter(x => x < 1);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  return names.filter(x => x.charAt(0) === char);
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  return words.filter(word => word.charAt(0) === 't' && word.charAt(1) === 'o' && word.charAt(2) === ' ');
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  return nums.filter(x => Number.isInteger(x));
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  let allCities = [];
+  for (let i = 0; i < users.length; i++) {
+    allCities[i] = users[i].data.city.displayName;
+  }
+  return allCities.flat();
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  return nums.map(x => parseFloat(Math.sqrt(x).toFixed(2)));
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  let regx = new RegExp(str.toLowerCase());
+  return sentences.filter(x => x.toLowerCase().match(regx));
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  let results = [];
+  for (let i = 0; i < triangles.length; i++) {
+    results[i] = Math.max(...triangles[i]);
+  }
+  return results;
 }
 
 module.exports = {
